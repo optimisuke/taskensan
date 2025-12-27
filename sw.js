22badf76d9f92cf59e21216ece55cbcbeb704545
optimisuke <Naosuke.Ito@ibm.com>
2025-12-27 22:02:34 +0900

    mod

---
+++ sw.js
@@ -0,0 +1,42 @@
+const CACHE = "tasken-san-cache-v1";
+const ASSETS = [
+  ".",
+  "index.html",
+  "manifest.json",
+  "icon.svg",
+  "santa.svg"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((key) => key !== CACHE).map((key) => caches.delete(key)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+      return fetch(event.request)
+        .then((response) => {
+          if (response && response.ok) {
+            const copy = response.clone();
+            caches.open(CACHE).then((cache) => cache.put(event.request, copy));
+          }
+          return response;
+        })
+        .catch(() => caches.match("index.html"));
+    })
+  );
+});
